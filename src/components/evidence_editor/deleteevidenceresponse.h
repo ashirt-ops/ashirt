65c99a936665857d31ae204a08e9feaebb30d7f4
Chris Rizzitello <crizzitello@ics.com>
2022-05-02 22:42:16 -0400

    Cleaner evidence Responces

--- src/components/evidence_editor/deleteevidenceresponse.h
+++ src/components/evidence_editor/deleteevidenceresponse.h
@@ -5,16 +5,16 @@
 #include "models/evidence.h"
 
 struct DeleteEvidenceResponse {
-  DeleteEvidenceResponse(model::Evidence model) {
-    this->model = model;
-  }
-  DeleteEvidenceResponse(bool fileDeleteSuccess, bool dbDeleteSuccess, QString err,
-                         model::Evidence model)
-      : DeleteEvidenceResponse(model) {
-    this->fileDeleteSuccess = fileDeleteSuccess;
-    this->dbDeleteSuccess = dbDeleteSuccess;
-    this->errorText = err;
-  }
+  DeleteEvidenceResponse(model::Evidence model)
+    : model(model){ }
+
+  DeleteEvidenceResponse(bool fileDeleteSuccess, bool dbDeleteSuccess,
+                                        QString err, model::Evidence model)
+    : model( model)
+    , fileDeleteSuccess(fileDeleteSuccess)
+    , dbDeleteSuccess(dbDeleteSuccess)
+    , errorText(err) { }
+
   bool fileDeleteSuccess;
   bool dbDeleteSuccess;
   QString errorText;
