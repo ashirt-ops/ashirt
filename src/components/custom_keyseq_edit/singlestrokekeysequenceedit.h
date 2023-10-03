febb90c2c9d71c721c357860dfacc67735a836e6
Chris Rizzitello <crizzitello@ics.com>
2023-10-03 11:19:41 -0400

    SingleStrokeKeySequenceEdit: Emit keychange and show clear Button remove eventfilter

--- src/components/custom_keyseq_edit/singlestrokekeysequenceedit.h
+++ src/components/custom_keyseq_edit/singlestrokekeysequenceedit.h
@@ -1,18 +1,16 @@
 #pragma once
 
-#include <QObject>
 #include <QKeySequenceEdit>
 #include <QKeyEvent>
 
 class SingleStrokeKeySequenceEdit : public QKeySequenceEdit
 {
+ Q_OBJECT
  public:
   SingleStrokeKeySequenceEdit(QWidget* parent=nullptr);
 
  protected:
   void keyPressEvent(QKeyEvent *evt) override;
-
-  bool eventFilter(QObject *object, QEvent *event) override;
 
  private:
   QKeySequence previousSequence;
