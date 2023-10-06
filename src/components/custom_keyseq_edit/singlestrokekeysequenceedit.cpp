51d8f9d96a3741fcd75ba38bd139333384553047
Chris Rizzitello <crizzitello@ics.com>
2023-10-06 08:45:48 -0400

    Further Simplify singlestrokekeysequence

--- src/components/custom_keyseq_edit/singlestrokekeysequenceedit.cpp
+++ src/components/custom_keyseq_edit/singlestrokekeysequenceedit.cpp
@@ -1,14 +1,10 @@
 #include "singlestrokekeysequenceedit.h"
+#include <QAbstractButton>
+#include <QLineEdit>
 
 SingleStrokeKeySequenceEdit::SingleStrokeKeySequenceEdit(QWidget* parent) : QKeySequenceEdit(parent){
   setClearButtonEnabled(true);
-}
-
-// Note: this may prevent editingFinished from firing
-void SingleStrokeKeySequenceEdit::keyPressEvent(QKeyEvent * evt) {
-  QKeySequenceEdit::keyPressEvent(evt);
-
-  setKeySequence(keySequence()[0]);
-  previousSequence = keySequence(); // update the saved sequence once one has been set
-  Q_EMIT keySequenceChanged(previousSequence);
+  setMaximumSequenceLength(1);
+  findChild<QLineEdit*>()->setReadOnly(true);
+  findChild<QLineEdit*>()->findChild<QAbstractButton*>()->setEnabled(true);
 }
--- src/components/custom_keyseq_edit/singlestrokekeysequenceedit.h
+++ src/components/custom_keyseq_edit/singlestrokekeysequenceedit.h
@@ -1,17 +1,10 @@
 #pragma once
 
 #include <QKeySequenceEdit>
-#include <QKeyEvent>
 
 class SingleStrokeKeySequenceEdit : public QKeySequenceEdit
 {
  Q_OBJECT
  public:
   SingleStrokeKeySequenceEdit(QWidget* parent=nullptr);
-
- protected:
-  void keyPressEvent(QKeyEvent *evt) override;
-
- private:
-  QKeySequence previousSequence;
 };
